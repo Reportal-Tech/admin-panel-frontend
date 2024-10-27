33f2dbb8e8166d64eb996a464449610669ebd7fa
aayush <sharma31aayush@gmail.com>
2024-10-27 17:36:53 +0530

    feat(firebase): add .firebaserc for project configuration and create firebase.json for hosting setup chore(workflows): replace old deploy.yml with a new firebase-deploy.yml for streamlined deployment process feat(next.config.js): update CORS headers configuration for improved API access feat(public): add custom 404.html page for better user experience on missing pages
    feat(index.html): add initial Firebase Hosting setup page to provide a welcome message and instructions for users

--- next.config.js
+++ next.config.js
@@ -1,34 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
+  reactStrictMode: true,
+  output: 'export',
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
           {
-            source: "/api/:path*",
-            headers: [
-              {
-                key: "Access-Control-Allow-Origin",
-                value: "*", // Set your origin
-              },
-              {
-                key: "Access-Control-Allow-Methods",
-                value: "GET, POST, PUT, DELETE, OPTIONS",
-              },
-              {
-                key: "Access-Control-Allow-Headers",
-                value: "Content-Type, Authorization",
-              },
-            ],
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Set your origin
           },
-        ];
-    },
-    images: {
-        remotePatterns: [
-            {
-                hostname: 'reportal-media.s3.ap-south-1.amazonaws.com',
-                protocol: 'https',                
-            }
-        ]
-    }
-}
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'reportal-media.s3.ap-south-1.amazonaws.com',
+        protocol: 'https',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
